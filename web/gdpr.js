6ed2333761fc1492308dcf84d4254048690f61c0
imtuhinkhan <uiu.tuhin@gmail.com>
2023-08-09 14:06:54 +0600

    fi

---
+++ web/gdpr.js
@@ -0,0 +1,30 @@
+import { DeliveryMethod } from "@shopify/shopify-api";
+
+/**
+ * @type {{[key: string]: import("@shopify/shopify-api").WebhookHandler}}
+ */
+export default {
+  CUSTOMERS_DATA_REQUEST: {
+    deliveryMethod: DeliveryMethod.Http,
+    callbackUrl: "/api/webhooks",
+    callback: async (topic, shop, body, webhookId) => {
+      const payload = JSON.parse(body);
+    },
+  },
+
+  CUSTOMERS_REDACT: {
+    deliveryMethod: DeliveryMethod.Http,
+    callbackUrl: "/api/webhooks",
+    callback: async (topic, shop, body, webhookId) => {
+      const payload = JSON.parse(body);
+    },
+  },
+
+  SHOP_REDACT: {
+    deliveryMethod: DeliveryMethod.Http,
+    callbackUrl: "/api/webhooks",
+    callback: async (topic, shop, body, webhookId) => {
+      const payload = JSON.parse(body);
+    },
+  },
+};
